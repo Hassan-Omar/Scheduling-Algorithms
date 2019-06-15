e9df8e5e9c3adb3b170f7ff5885ba349baa60bca
Hassan Omar <ho1138@fayoum.edu.eg>
2019-06-15 11:06:48 -0700

    keeping progress in SRF

--- src/task/schedular/SRF.java
+++ src/task/schedular/SRF.java
@@ -2,21 +2,13 @@ package task.schedular;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.JOptionPane;
 
 /**
  *
  * @author h.omar
  */
-public class RR extends Algorithm {
- int q=1 ; 
-    public RR() {
-        this.setName("RR");
-       // read q from user 
-       String s = JOptionPane.showInputDialog("Enter Q");
-         q =(int)Float.parseFloat(s) ;
-    }
-    
+public class SRF extends Algorithm {
+
 
   //----------------------------------------------------------------------- 
    public List<Task>  drive (List<Task> tasks)
@@ -35,55 +27,56 @@ public class RR extends Algorithm {
        
        }
        
-          // now we need to set start and end time based on RR
-          // |_A_|_C_|_A_|_B_|_D_|_A_|_C_|_D_|_A_|_C_|_A_| 
+          // now we need to set start and end time based on SRF
+          // |___A___|__C__|_A_|_B_|____D____|_C_| 
           // start of TASK(i+1) = end of  TASK(i)  
-          for(int index=0;index<sum_Burst; index+=q)
-          {
-              int i =0 ;
-          if(i==0)
+          for(int i=0;i<sum_Burst; i++)
+          { int remaining = result_list.get(i).getBurstTime() ;
+             
+             if(i==0)
               {
                 // first task is the first arrived
                 // and it's Start Time = arrival time 
                  tasks_Arri.get(0).setStartTime(tasks_Arri.get(0).getArrivalTime());
-                 queu.add(tasks_Arri.get(0));
+                // queu.add(tasks_Arri.get(0)) ;
                  result_list.add(tasks_Arri.get(0)) ;
                   
                  
               }
-              else 
+              
+              
+              for(int j=0 ;j<sum_Burst; j++)
               { 
-                 // now we need to move queu(0) to the last
-                 for (int x=0; x<tasks.size(); x++)
-                 {
-                     if(tasks_Arri.get(x).getArrivalTime() < i)
-                     {
-                     // this means it's delevird
-                     queu.add(tasks_Arri.get(x));
-                     }
-                 }
-                  result_list.add(queu.get(0));
+                  if(tasks_Arri.get(j).getArrivalTime()==j)
+                  {
+                   // stop the currnet task if the comming burst < remaining
+                  if(tasks_Arri.get(j).getBurstTime() < remaining)
+                  {
+                      tasks_Arri.get(j).setStartTime(j);
+                      
+                      result_list.add(tasks_Arri.get(j));
+                      // move the task in queu
+                      tasks_Arri.get(j).setRemainTime(remaining);
+                      tasks_Arri.add(j, tasks_Arri.get(j));
+                      tasks_Arri.remove(0); 
+                      // breaking inner loop
+                      j+=sum_Burst ;
+                  }
+                 
                   
-              } 
-            System.out.println(i);
-          // remain time = burst - q
-            queu.get(0).setRemainTime( result_list.get(i).getBurstTime()-q);
-          
-          // check if the task @ end
-          if(queu.get(0).getRemainTime()==0)
-          queu.remove(queu.get(0));
-          // end = start + q
-            result_list.get(i).setEndTime( result_list.get(i).getStartTime()+q );
-
-          if(i>0)
-          {
-          // roatate the queue
-          queu.add(queu.get(0));
-          queu.remove(0);
-         }
+                  }
+              
+               remaining--;
+              
+              }
+           // end time = start + busy = start + burst - remaining = end of privous + burst - remaining    
+ result_list.get(i).setEndTime(result_list.get(i-1).getEndTime()+ ( result_list.get(i-1).getBurstTime()- result_list.get(i-1).getRemainTime()));   
           System.out.println("end "+  result_list.get(i).getEndTime() +"start"+  result_list.get(i).getStartTime());
-         i++ ;
+         
           }
+          
+          
+          
         List<Task> tasks_Setted =   UtileMethods.setParam(result_list) ;
   
         // start calculation using the parent methods 
@@ -97,4 +90,5 @@ public class RR extends Algorithm {
       return tasks_Setted ;
   
    } 
+    
 }
